db87efc947e20e71f2029d44da50691a111ffb48
Mojopon <tyamuro@gmail.com>
2016-01-20 09:29:23 +0900

    add digital timer text converter
    add digital timer text converter

--- LaLaTimer/Converter/DigitalTimerTextConverter.cs
+++ LaLaTimer/Converter/DigitalTimerTextConverter.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace LaLaTimer.Converter
 {
-    public class DigitalTimerTextConverter
+    public class DigitalTimerTextConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = (int)value;
+
+            string timeText = v.ToString();
+            if(v < 10)
+            {
+                timeText = timeText.Insert(0, "0");
+            }
+
+            return timeText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
